77605f0f96c0ccfe345a4fb6521ddce48519e3a5
Samhita-sopho <samhitak@hotmail.com>
2017-09-27 01:29:46 +0530

    codec update

--- EDOS_PROJECT/read_write_videos.py
+++ EDOS_PROJECT/read_write_videos.py
@@ -1,7 +1,17 @@
 import numpy as np
 import cv2
+filename='C:\\edited_videos\\2try.mp4'
+cap = cv2.VideoCapture(filename)
 
-cap = cv2.VideoCapture('C:\\moviepy-master\\moviepy-master\\lecture7.mp4')
+extension=filename.split(".")[1]
+
+#finding the codec
+if extension=="mp4":
+     codec='H264'
+elif extension=="avi":
+     codec='XVID'
+     
+
 #finding framerate
 
 #finds the version of opencv
@@ -18,14 +28,16 @@ else :
         print "Frames per second using video.get(cv2.CAP_PROP_FPS) : {0}".format(fps)
 
 
-# Define the codec and create VideoWriter object
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('C:\\moviepy-master\\moviepy-master\\outs.mp4',fourcc, fps, (640,480))
 
+# Define the codec and create VideoWriter object
+fourcc = cv2.VideoWriter_fourcc(*codec)
+out = cv2.VideoWriter('C:\\edited_videos\\outs.mp4',fourcc, fps, (long(cap.get(3)),long(cap.get(4))))#,480)
+counter=0
 while(cap.isOpened()):
     ret, frame = cap.read()
     if ret==True:
         out.write(frame)
+        counter=counter+1;
 
         cv2.imshow('frame',frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -37,3 +49,4 @@ while(cap.isOpened()):
 cap.release()
 #out.release()
 cv2.destroyAllWindows()
+print(counter)
